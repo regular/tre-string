fa9cd24dfd1679a56f2eaa9dd717a6873a42dd53
Jan Bölsche <regular.gonzales@googlemail.com>
2018-11-07 13:40:51 +0100

    demo

---
+++ demo.js
@@ -0,0 +1,5 @@
+const Str = require('.')
+
+const render = Str({})
+
+document.body.appendChild(render('Hello World'))
---
+++ index.js
@@ -0,0 +1,63 @@
+const h = require('mutant/html-element')
+const Value = require('mutant/value')
+
+function selectAll(el) {
+  const range = document.createRange()
+  range.selectNodeContents(el)
+  const sel = window.getSelection()
+  sel.removeAllRanges()
+  sel.addRange(range)
+}
+
+module.exports = function(opts) {
+  opts = opts || {}
+  const canEdit = opts.canEdit || function() {return true}
+
+  return function(value, ctx) {
+    const editing = Value(false)
+    let el
+
+    function save() {
+      value=el.innerText
+      editing.set(false)
+      el.blur()
+      if (opts.save) opts.save(el.innerText, el)
+    }
+    
+    function cancel() {
+      editing.set(false)
+      el.innerText=value
+      el.blur()
+    }
+    
+    el = h('span', {
+      attributes: {
+        contenteditable: editing,
+      },
+      'ev-click': e => {
+        if (!editing() && canEdit(e.target)) {
+          editing.set(true)
+          el.focus()
+          selectAll(el)
+        }
+      },
+      'ev-blur': e => {
+        if (editing()) {
+          save()
+        }
+      },
+      'ev-keydown': e => {
+        if (!editing()) return
+        if (e.key == 'Escape') {
+          cancel()
+          e.preventDefault()
+        }
+        if (e.key == 'Enter') {
+          save()
+          e.preventDefault()
+        }
+      }
+    }, value)
+    return el
+  }
+}
